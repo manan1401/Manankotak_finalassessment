bd1a25518021ed0944ceaeb40bd6f21d3f095685
mananmk <manankotak182000@gmail.com>
2021-03-11 11:23:08 +0530

    This is unity's final exam.I have given my best and completed all the tasks.Thank you sir

---
+++ Manan_finalassessment/Assets/color_dynamic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color_dynamic : MonoBehaviour
+{
+    Color[] colors = new Color[4];
+    // Start is called before the first frame update
+    void Start()
+    {
+        colors[0] = Color.red;
+        colors[1] = Color.blue;
+        colors[2] = Color.green;
+        colors[3] = Color.yellow;
+
+        GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
